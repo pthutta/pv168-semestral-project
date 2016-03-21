1d50ba3d944208352f60fec572c7aebbdfd55622
cechy <d.cechak@seznam.cz>
2016-03-21 18:39:52 +0100

    CauldronManagerImpl zkopirovane z předlohy a přepsané na naše názvy/metody ToDo:  - nakonfigurovat databazi?             - pochopit a poupravit kód ať nejsme jak copymachine a ať to nevypadá blbě

---
+++ src/main/java/cz/muni/fi/pv168/impl/EntityNotFoundException.java
@@ -0,0 +1,16 @@
+package cz.muni.fi.pv168.impl;
+
+/**
+ * Created by cechy on 21.03.2016.
+ */
+public class EntityNotFoundException extends RuntimeException {
+
+    /**
+     * Constructs an instance of <code>EntityNotFoundException</code> with the specified detail message.
+     *
+     * @param msg the detail message.
+     */
+    public EntityNotFoundException(String msg) {
+        super(msg);
+    }
+}
