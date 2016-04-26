f87121f21be7893371190fe446ec58f761e2ec2e
cechy <d.cechak@seznam.cz>
2016-04-26 19:46:53 +0200

    základ GUI: Menu, Sinners, Cauldrons, Add u obojího  - u AddSinner funguje toggle->disable date field a naopak  - ještě dodelat update?  - dodělat kalendář

---
+++ Gui/src/main/java/cz/muni/fi/pv168/gui/Sinners.java
@@ -0,0 +1,58 @@
+package cz.muni.fi.pv168.gui;
+
+import javax.swing.*;
+import java.awt.event.*;
+
+public class Sinners extends JDialog {
+    private JPanel contentPane;
+    private JButton buttonOK;
+    private JButton deleteButton;
+    private JTable table1;
+    private JButton updateButton;
+    private JButton button1;
+
+    public Sinners() {
+        setContentPane(contentPane);
+        setModal(true);
+        getRootPane().setDefaultButton(buttonOK);
+
+        buttonOK.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                onOK();
+            }
+        });
+
+// call onCancel() when cross is clicked
+        setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+        addWindowListener(new WindowAdapter() {
+            public void windowClosing(WindowEvent e) {
+                onCancel();
+            }
+        });
+
+// call onCancel() on ESCAPE
+        contentPane.registerKeyboardAction(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                onCancel();
+            }
+        }, KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
+        button1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                AddSinner dialog = new AddSinner();
+                dialog.pack();
+                dialog.setVisible(true);
+            }
+        });
+    }
+
+    private void onOK() {
+// add your code here
+        dispose();
+    }
+
+    private void onCancel() {
+// add your code here if necessary
+        dispose();
+    }
+}
